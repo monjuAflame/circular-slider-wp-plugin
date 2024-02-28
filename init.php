73ff98f2f0e94f4d692be04164ad658a864935f8
monjuaflame <monjuaflame@gmail.com>
2024-02-28 11:54:50 +0600

    setup

---
+++ init.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Circular Slider
+Plugin URI: 
+Description: Responsive Slider, 
+Version: 1.0.0
+Author: ismaildev
+Author URI: http://www.ismailhossaindev.Com/
+Text Domain: cir_slider
+Domain Path: /lang
+*/
